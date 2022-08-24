9235fb468bc3282bc0474395068fe5f38850f61d
AruKesha <arukeshxo@gmail.com>
2022-08-24 17:18:04 +0600

    Self-made hw as usual, Airas

---
+++ lesson6_git.py
@@ -0,0 +1,31 @@
+"""
+Git
+GitHub
+GitLab
+
+"""
+
+"""
+
+CLI = Command  Line Interface
+"""
+
+"""
+Algorithm 
+"""
+
+"""Line Search """
+# arr = list(range(1, 10))
+# for i in arr:
+#     print(i)
+
+""" Big O - сложность алгоритма """
+
+# mass = [1, 2, 3, 4]
+# num = int(input())
+#
+# for item in range(len(mass)):
+#     if item == 2:
+#         print(f'Item {mass[item]}')
+#         break
+"""Big O(n->len(massive))"""
